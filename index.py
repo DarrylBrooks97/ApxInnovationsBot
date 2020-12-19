abc46154612a2405815302c56a8617a367314383
Darryl Brooks <darrylbrooks13@gmail.com>
2020-12-19 10:33:22 -0500

    Changing tracking wording

--- index.py
+++ index.py
@@ -51,4 +51,4 @@ api = tweepy.API(auth, wait_on_rate_limit=True,
     wait_on_rate_limit_notify=True)
 tweets_listener = MyStreamListener(api)
 stream = tweepy.Stream(api.auth, tweets_listener)
-stream.filter(track=["What's my profile status?","Whats my profile status?","What's my profile status","Whats my profile status"], languages=["en"],is_async=True)
+stream.filter(track=["#Whatsmyprofilestats","#Whatsmyprofilestatus"], languages=["en"],is_async=True)
